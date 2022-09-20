503d2734ffdffffd64cf21713e8c5617aa21f9d0
jay2viv <97814633+jay2viv@users.noreply.github.com>
2022-09-20 18:29:26 -0400

    Add files via upload

---
+++ main3.js
@@ -0,0 +1,27 @@
+function friedrice()
+{
+    window.location = "index4.html";
+}
+
+function dessert_kidmeal()
+{
+    window.location = "index5.html";
+}
+
+function curry()
+{
+    window.location.replace("index3.html");
+}
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCMJ73oyzTN5Bbsixb4d_QVFtsnxJd7PQI",
+    authDomain: "dronedrop-eb819.firebaseapp.com",
+    databaseURL: "https://dronedrop-eb819-default-rtdb.firebaseio.com",
+    projectId: "dronedrop-eb819",
+    storageBucket: "dronedrop-eb819.appspot.com",
+    messagingSenderId: "513030965756",
+    appId: "1:513030965756:web:54150c301b1afa493b8a79"
+  };
+  
+  
+  const app = initializeApp(firebaseConfig);
